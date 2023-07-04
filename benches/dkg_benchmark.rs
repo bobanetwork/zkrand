cfb0e5b62ecde97a7b0d4a953f9fd354a55c6a22
Jia Liu <lovelykitoun@gmail.com>
2023-07-04 14:03:28 +0100

    add benchmarks

---
+++ benches/dkg_benchmark.rs
@@ -0,0 +1,106 @@
+use criterion::{criterion_group, Criterion};
+
+mod dkg_benches {
+    use super::*;
+    use ark_std::{end_timer, start_timer};
+    use halo2wrong::curves::bn256::{Bn256, G1Affine as BnG1};
+    use halo2wrong::halo2::plonk::{create_proof, keygen_pk, keygen_vk, verify_proof};
+    use halo2wrong::halo2::poly::commitment::ParamsProver;
+    use halo2wrong::halo2::poly::kzg::commitment::{
+        KZGCommitmentScheme, ParamsKZG, ParamsVerifierKZG,
+    };
+    use halo2wrong::halo2::poly::kzg::multiopen::{ProverSHPLONK, VerifierSHPLONK};
+    use halo2wrong::halo2::poly::kzg::strategy::SingleStrategy;
+    use halo2wrong::halo2::transcript::{
+        Blake2bRead, Blake2bWrite, Challenge255, TranscriptReadBuffer, TranscriptWriterBuffer,
+    };
+    use rand_core::OsRng;
+    use zkdvrf::{CircuitDkg, DkgMemberParams, MemberKey};
+
+    fn dkg_proof_verify<
+        const THRESHOLD: usize,
+        const NUMBER_OF_MEMBERS: usize,
+        const DEGREE: usize,
+    >(
+        c: &mut Criterion,
+    ) {
+        let mut rng = OsRng;
+
+        let mut members = vec![];
+        let mut pks = vec![];
+        for _ in 0..NUMBER_OF_MEMBERS {
+            let member = MemberKey::new(&mut rng);
+            pks.push(member.get_public_key());
+            members.push(member);
+        }
+
+        let dkg_params =
+            DkgMemberParams::<THRESHOLD, NUMBER_OF_MEMBERS>::new(1, &pks, &mut rng).unwrap();
+        let circuit = dkg_params.get_circuit(&mut rng);
+        let instance = dkg_params.get_instance();
+        let instance_ref = instance.iter().map(|i| i.as_slice()).collect::<Vec<_>>();
+
+        let start1 = start_timer!(|| format!("kzg setup with degree {}", DEGREE));
+        let general_params = ParamsKZG::<Bn256>::setup(DEGREE as u32, &mut rng);
+        let verifier_params: ParamsVerifierKZG<Bn256> = general_params.verifier_params().clone();
+        end_timer!(start1);
+
+        let vk = keygen_vk(&general_params, &circuit).expect("keygen_vk should not fail");
+        let pk = keygen_pk(&general_params, vk, &circuit).expect("keygen_pk should not fail");
+
+        let mut transcript = Blake2bWrite::<_, BnG1, Challenge255<_>>::init(vec![]);
+
+        let proof_message = format!("dkg proof with degree = {}", DEGREE);
+        let start2 = start_timer!(|| proof_message);
+        create_proof::<
+            KZGCommitmentScheme<Bn256>,
+            ProverSHPLONK<'_, Bn256>,
+            Challenge255<BnG1>,
+            OsRng,
+            Blake2bWrite<Vec<u8>, BnG1, Challenge255<BnG1>>,
+            CircuitDkg<THRESHOLD, NUMBER_OF_MEMBERS>,
+        >(
+            &general_params,
+            &pk,
+            &[circuit],
+            &[instance_ref.as_slice()],
+            rng,
+            &mut transcript,
+        )
+        .expect("proof generation should not fail");
+        let proof = transcript.finalize();
+        end_timer!(start2);
+
+        c.bench_function("dkg proof verification", move |b| {
+            b.iter(|| {
+                let mut verifier_transcript =
+                    Blake2bRead::<_, BnG1, Challenge255<_>>::init(&proof[..]);
+                let strategy = SingleStrategy::new(&general_params);
+                verify_proof::<
+                    KZGCommitmentScheme<Bn256>,
+                    VerifierSHPLONK<'_, Bn256>,
+                    Challenge255<BnG1>,
+                    Blake2bRead<&[u8], BnG1, Challenge255<BnG1>>,
+                    SingleStrategy<'_, Bn256>,
+                >(
+                    &verifier_params,
+                    pk.get_vk(),
+                    strategy,
+                    &[instance_ref.as_slice()],
+                    &mut verifier_transcript,
+                )
+                .expect("failed to verify dkg circuit")
+            })
+        });
+    }
+
+    criterion_group! {
+        name = dkg_benches;
+        config = Criterion::default();
+        targets =
+            dkg_proof_verify::<4,6,20>,
+    //        dkg_proof_verify::<7,13,21>,
+    }
+}
+
+criterion::criterion_main!(dkg_benches::dkg_benches);
---
+++ benches/dvrf_benchmark.rs
@@ -0,0 +1,113 @@
+use criterion::{criterion_group, Criterion};
+
+mod dvrf_benches {
+    use super::*;
+    use blake2b_simd::blake2b;
+    use halo2wrong::curves::bn256::{Fr as BnScalar, G1Affine as BnG1, G2Affine as BnG2};
+    use halo2wrong::curves::group::{Curve, GroupEncoding};
+    use halo2wrong::halo2::arithmetic::Field;
+    use rand_core::OsRng;
+    use zkdvrf::{
+        combine_partial_evaluations, get_shares, hash_to_curve, keygen, DkgShareKey, PseudoRandom,
+        EVAL_PREFIX,
+    };
+
+    // partial evaluation time independent of the values of threshold and number of members
+    fn partial_evaluate(c: &mut Criterion) {
+        let mut rng = OsRng;
+
+        let index = 1;
+        let (sk, vk) = keygen(&mut rng);
+        let key = DkgShareKey::<4, 6>::new(index, sk, vk);
+        let x = b"the first random 20230703";
+
+        c.bench_function("dvrf partial evaluation", move |b| {
+            b.iter(|| key.evaluate(x, &mut rng))
+        });
+    }
+
+    fn combine<const THRESHOLD: usize, const NUMBER_OF_MEMBERS: usize>(c: &mut Criterion) {
+        let mut rng = OsRng;
+
+        let g = BnG1::generator();
+        let coeffs: Vec<_> = (0..THRESHOLD).map(|_| BnScalar::random(&mut rng)).collect();
+        let shares = get_shares::<THRESHOLD, NUMBER_OF_MEMBERS>(&coeffs);
+        let keys: Vec<_> = shares
+            .iter()
+            .enumerate()
+            .map(|(i, s)| {
+                DkgShareKey::<THRESHOLD, NUMBER_OF_MEMBERS>::new(i + 1, *s, (g * s).to_affine())
+            })
+            .collect();
+        let vks: Vec<_> = keys.iter().map(|key| key.get_verification_key()).collect();
+
+        let input = b"test first random";
+
+        let evals: Vec<_> = keys
+            .iter()
+            .map(|key| key.evaluate(input, &mut rng))
+            .collect();
+
+        let res = evals
+            .iter()
+            .zip(vks.iter())
+            .all(|(e, vk)| e.verify(input, vk).is_ok());
+
+        assert!(res);
+
+        let name = format!("dvrf combine partial evaluation ({THRESHOLD}, {NUMBER_OF_MEMBERS})");
+        c.bench_function(name.as_str(), move |b| {
+            b.iter(|| combine_partial_evaluations(&evals[0..THRESHOLD]).unwrap());
+        });
+    }
+
+    fn partial_verify(c: &mut Criterion) {
+        let mut rng = OsRng;
+
+        let index = 1;
+        let (sk, vk) = keygen(&mut rng);
+        // verification time of partial evaluation independent of the values of threshold and number of members
+        let key = DkgShareKey::<4, 6>::new(index, sk, vk);
+        let input = b"the first random 20230703";
+        let sigma = key.evaluate(input, &mut rng);
+
+        c.bench_function("dvrf partial evaluation verify", move |b| {
+            b.iter(|| sigma.verify(input, &vk).unwrap())
+        });
+    }
+
+    fn pseudo_random_verify(c: &mut Criterion) {
+        let mut rng = OsRng;
+        let (a, _) = keygen(&mut rng);
+        let g2 = BnG2::generator();
+        let gpk = (g2 * a).to_affine();
+
+        let input = b"the first random 20230703";
+        let hasher = hash_to_curve(EVAL_PREFIX);
+        let h: BnG1 = hasher(input).to_affine();
+
+        let proof = (h * a).to_affine();
+        let value = blake2b(proof.to_bytes().as_ref()).as_bytes().to_vec();
+        let pr = PseudoRandom::new(proof, value);
+
+        c.bench_function("dvrf pseudorandom verification", move |b| {
+            b.iter(|| pr.verify(input, &gpk).unwrap())
+        });
+    }
+
+    criterion_group! {
+        name = dvrf_benches;
+        config = Criterion::default();
+        targets =
+            partial_evaluate,
+            combine::<4,6>,
+            combine::<7,13>,
+            combine::<14,27>,
+            combine::<28,55>,
+            combine::<57,112>,
+            partial_verify,
+            pseudo_random_verify,
+    }
+}
+
+criterion::criterion_main!(dvrf_benches::dvrf_benches);
