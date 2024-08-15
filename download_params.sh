2331f84f832e10c9ab15c83f303000e5dc747620
Jia Liu <jia@enya.ai>
2024-08-15 11:01:47 +0100

    change download method

--- download_params.sh
+++ download_params.sh
@@ -10,4 +10,4 @@ degree_output_file="$params_dir"/params"${degree}"
 rm -f "$degree_output_file"
 
 # https://docs.axiom.xyz/docs/transparency-and-security/kzg-trusted-setup
-axel -ac https://axiom-crypto.s3.amazonaws.com/challenge_0085/kzg_bn254_"${degree}".srs -o "$degree_output_file"
+curl -f -o "$degree_output_file" "https://axiom-crypto.s3.amazonaws.com/challenge_0085/kzg_bn254_${degree}.srs"
